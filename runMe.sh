665bbb5339ee4a1efe251d752311a4f5041409e8
James Casaletto <jcasaletto@maprtech.com>
2019-09-16 14:57:35 -0700

    fixed 'no module found' bug

--- runMe.sh
+++ runMe.sh
@@ -8,5 +8,7 @@ DATA_PATH=${FDA_PATH}/data
 
 DOCKER_IMAGE_NAME=my_fda_image
 
-docker run --rm -e PYTHONIOENCODING=UTF-8 -w /app --user=`id -u`:`id -g` -v ${APP_PATH}:/app:ro -v ${CONF_PATH}:/config -v "${DATA_PATH}":/data ${DOCKER_IMAGE_NAME} /usr/bin/python3 /app/dataAnalyzer.py /config/conf.json
+docker run --rm -e PYTHONPATH=/ -e PYTHONIOENCODING=UTF-8 -w / --user=`id -u`:`id -g` -v ${APP_PATH}:/app:ro -v ${CONF_PATH}:/config -v "${DATA_PATH}":/data ${DOCKER_IMAGE_NAME} /usr/bin/python3 /app/dataAnalyzer.py /config/conf.json
+
+#docker run -it -w /app --user=0:0 -v ${APP_PATH}:/app:ro -v ${CONF_PATH}:/config -v "${DATA_PATH}":/data --entrypoint /bin/bash ${DOCKER_IMAGE_NAME} 
 
