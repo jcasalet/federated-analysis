fb80b746802f6c4405ac04eb072a51281f8366c4
James Casaletto <james@brcaexchange-dev.gi.ucsc.edu>
2020-01-01 03:21:02 +0000

    testing from brca

--- runMe.sh
+++ runMe.sh
@@ -11,4 +11,3 @@ DOCKER_IMAGE_NAME=my_fda_image
 docker run --rm -e PYTHONPATH=/ -e PYTHONIOENCODING=UTF-8 -w / --user=`id -u`:`id -g` -v ${APP_PATH}:/app:ro -v ${CONF_PATH}:/config -v "${DATA_PATH}":/data ${DOCKER_IMAGE_NAME} /usr/bin/python3 /app/dataAnalyzer.py /config/conf.json
 
 #docker run -it -w /app --user=0:0 -v ${APP_PATH}:/app:ro -v ${CONF_PATH}:/config -v "${DATA_PATH}":/data --entrypoint /bin/bash ${DOCKER_IMAGE_NAME} 
-
